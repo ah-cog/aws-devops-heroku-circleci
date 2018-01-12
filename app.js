fdb999ae494827b02743cbca3d82459d86c68830
Michael Gubbels <mgub@mgub.co>
2018-01-12 16:57:17 -0600

    Demo.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require("express")
 var path = require("path")
-var favicon = require("serve-favicon")
 var logger = require("morgan")
 var cookieParser = require("cookie-parser")
 var bodyParser = require("body-parser")
@@ -13,12 +12,10 @@ var http = require("http")
 
 var app = express()
 
-// view engine setup
+// Configure Express view engine.
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -28,82 +25,31 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use("/", index)
 app.use("/users", users)
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler.
 app.use(function(req, res, next) {
 	var err = new Error("Not Found")
 	err.status = 404
 	next(err)
 })
 
-// error handler
+// Error handler.
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
+	// Set locals, only providing error in development.
 	res.locals.message = err.message
 	res.locals.error = req.app.get("env") === "development" ? err : {}
 
-	// render the error page
+	// Render the error page.
 	res.status(err.status || 500)
 	res.render("error")
 })
 
-//----------
-
-/**
- * Get port from environment and store in Express.
- */
-
+// Set port from environment.
 var port = parseInt(process.env.PORT, 10) || 5000
 // var port = 5000
 app.set("port", port)
 
-/**
- * Create HTTP server.
- */
-
+// Create HTTP server.
 var server = http.createServer(app)
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
+// Listen on provided port, on all network interfaces.
 server.listen(port)
-// server.on("error", onError)
-// server.on("listening", onListening)
-
-// /**
-//  * Event listener for HTTP server "error" event.
-//  */
-
-// function onError(error) {
-// 	if (error.syscall !== "listen") {
-// 		throw error
-// 	}
-
-// 	var bind = typeof port === "string" ? "Pipe " + port : "Port " + port
-
-// 	// handle specific listen errors with friendly messages
-// 	switch (error.code) {
-// 		case "EACCES":
-// 			console.error(bind + " requires elevated privileges")
-// 			process.exit(1)
-// 			break
-// 		case "EADDRINUSE":
-// 			console.error(bind + " is already in use")
-// 			process.exit(1)
-// 			break
-// 		default:
-// 			throw error
-// 	}
-// }
-
-// /**
-//  * Event listener for HTTP server "listening" event.
-//  */
-
-// function onListening() {
-// 	var addr = server.address()
-// 	var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port
-// 	debug("Listening on " + bind)
-// }
-
-// module.exports = app
